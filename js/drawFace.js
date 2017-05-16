1464b1d7523c18d755581f3ed8a158668040b227
Eric Phillips <dephil29@hotmail.com>
2017-05-16 15:53:35 -0400

    changed colors and hid my name

--- js/drawFace.js
+++ js/drawFace.js
@@ -2,17 +2,17 @@ const drawFace = function (ctx, radius) {
   var grad;
   ctx.beginPath();
   ctx.arc(0, 0, radius, 0, 2*Math.PI);
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = '#f4f4f4';
   ctx.fill();
   grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
-  grad.addColorStop(0, '#333');
-  grad.addColorStop(0.5, 'white');
-  grad.addColorStop(1, '#333');
+  grad.addColorStop(0, '#ee2560');
+  grad.addColorStop(0.5, '#f4f4f4');
+  grad.addColorStop(1, '#ee2560');
   ctx.strokeStyle = grad;
   ctx.lineWidth = radius*0.1;
   ctx.stroke();
   ctx.beginPath();
   ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
-  ctx.fillStyle = '#333';
+  ctx.fillStyle = '#ee2560';
   ctx.fill();
 }
